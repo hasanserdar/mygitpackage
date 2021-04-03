78e6a7b0d5d138b256040535a59c7960cbd9a12b
Hasan <sargin.hasanserdar@gmail.com>
2021-04-03 18:54:19 +0200

    version03

--- src/mygitpackage/JavaGitClass.java
+++ src/mygitpackage/JavaGitClass.java
@@ -5,6 +5,7 @@ public class JavaGitClass {
 	public static void main(String[] args) {
 		System.out.println("Version 1");
 		System.out.println("My Second Commit");
+		System.out.println("My Third Commit");
 		
 		
 	
